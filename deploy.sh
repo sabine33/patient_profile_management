e59ed9a11d0c6b9295883030825acf58091b3201
Sabin Khanal <sabin.khanal.33@gmail.com>
2022-11-29 21:57:03 +0545

    Build updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # yarn build && cd build && git init && git add . && git commit -m "Deployment release" && git remote add production ssh://techbyte@23.106.120.XXX/home/techbyte/ppm.git && git push production master --force
 rm -r dist/
-yarn build && git add . && git commit -m "Build updates" && git push origin staging
+yarn build && git add . && git commit -m "Build updated" && git push origin staging
