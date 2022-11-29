748b25a66d5037fa36a4cab44ba8f173706072ac
Sabin Khanal <sabin.khanal.33@gmail.com>
2022-11-29 10:58:01 +0545

    Build updates

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const IMAGE_URL = import.meta.env.VITE_API_URL + "/uploads";
+// export const IMAGE_URL = import.meta.env.VITE_API_URL + "/uploads";
+export const IMAGE_URL = "https://ppm-api.onrender.com" + "/uploads";
